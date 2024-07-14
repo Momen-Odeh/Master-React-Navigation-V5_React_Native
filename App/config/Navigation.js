123173504a5d299ec8971058b0bcc575865a461d
Momen-Odeh <momen.odeh74@gmail.com>
2024-07-14 16:58:14 +0300

    add Navigation tab

--- App/config/Navigation.js
+++ App/config/Navigation.js
@@ -1,8 +1,11 @@
 import { NavigationContainer } from "@react-navigation/native";
 import { createStackNavigator } from "@react-navigation/stack";
+import { createBottomTabNavigator } from "@react-navigation/bottom-tabs";
 
 import ContactsList from "../screens/ContactsList";
 import ContactDetails from "../screens/ContactDetails";
+import ActionsList from "../screens/ActionsList";
+import ActionDetails from "../screens/ActionDetails";
 
 const ContactsStack = createStackNavigator();
 const ContactsStackScreen = () => (
@@ -30,9 +33,25 @@ const ContactsStackScreen = () => (
   </ContactsStack.Navigator>
 );
 
+const ActionsStack = createStackNavigator();
+const ActionsStackScreen = () => (
+  <ActionsStack.Navigator>
+    <ActionsStack.Screen name="ActionsList" component={ActionsList} />
+    <ActionsStack.Screen name="ActionDetails" component={ActionDetails} />
+  </ActionsStack.Navigator>
+);
+
+const AppTabs = createBottomTabNavigator();
+const AppTabsScreen = () => (
+  <AppTabs.Navigator>
+    <AppTabs.Screen name="Contacts" component={ContactsStackScreen} />
+    <AppTabs.Screen name="Actions" component={ActionsStackScreen} />
+  </AppTabs.Navigator>
+);
+
 const Navigation = () => (
   <NavigationContainer>
-    <ContactsStackScreen />
+    <AppTabsScreen />
   </NavigationContainer>
 );
 
