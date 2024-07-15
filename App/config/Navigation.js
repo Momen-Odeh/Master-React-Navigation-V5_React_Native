854d84414530297c2a90f6759041a90d5a0e10ed
Momen-Odeh <momen.odeh74@gmail.com>
2024-07-15 09:45:14 +0300

    working with Tap Drawer Navigator

--- App/config/Navigation.js
+++ App/config/Navigation.js
@@ -6,7 +6,10 @@ import ContactsList from "../screens/ContactsList";
 import ContactDetails from "../screens/ContactDetails";
 import ActionsList from "../screens/ActionsList";
 import ActionDetails from "../screens/ActionDetails";
-
+import { AntDesign } from "@expo/vector-icons";
+import { Ionicons } from "@expo/vector-icons";
+import { createDrawerNavigator } from "@react-navigation/drawer";
+import Settings from "../screens/Settings";
 const ContactsStack = createStackNavigator();
 const ContactsStackScreen = () => (
   <ContactsStack.Navigator
@@ -43,15 +46,46 @@ const ActionsStackScreen = () => (
 
 const AppTabs = createBottomTabNavigator();
 const AppTabsScreen = () => (
-  <AppTabs.Navigator>
-    <AppTabs.Screen name="Contacts" component={ContactsStackScreen} />
-    <AppTabs.Screen name="Actions" component={ActionsStackScreen} />
+  <AppTabs.Navigator
+    screenOptions={
+      {
+        // tabBarLabel: "test",
+        // tabBarActiveTintColor: "red",
+        // tabBarActiveBackgroundColor: "green",
+      }
+    }
+  >
+    <AppTabs.Screen
+      name="Contacts"
+      component={ContactsStackScreen}
+      options={{
+        tabBarIcon: ({ size, color, focused }) => (
+          <AntDesign name="contacts" size={size} color={color} />
+        ),
+        // tabBarLabel: "Contacts",
+      }}
+    />
+    <AppTabs.Screen
+      name="Actions"
+      component={ActionsStackScreen}
+      options={{
+        tabBarIcon: ({ size, color }) => (
+          <Ionicons name="checkmark-circle-outline" size={size} color={color} />
+        ),
+      }}
+    />
   </AppTabs.Navigator>
 );
-
+const AppDrawer = createDrawerNavigator();
+const AppDrawerScreen = () => (
+  <AppDrawer.Navigator>
+    <AppDrawer.Screen name="Tabs" component={AppTabsScreen} />
+    <AppDrawer.Screen name="Settings" component={Settings} />
+  </AppDrawer.Navigator>
+);
 const Navigation = () => (
   <NavigationContainer>
-    <AppTabsScreen />
+    <AppDrawerScreen />
   </NavigationContainer>
 );
 
